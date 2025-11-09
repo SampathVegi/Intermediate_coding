7fabcde5eb702ba36a42b78cd6a9d6397d8f1249
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2025-11-10 00:16:06 +0530

    Time: 9 ms (23.53%) | Memory: 47.2 MB (8.12%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       HashMap<Integer,Integer> h = new HashMap<>();
-       for(int e:nums)
-            h.put(e,h.getOrDefault(e,0)+1);
-        int result =0;
-        for(int e : h.keySet()){
-        if(h.get(e)==1)
-            result =e;
-            }
-        return result; }
+       Arrays.sort(nums);
+       for(int i=0;i<nums.length-1;i+=2){
+        if(nums[i]!=nums[i+1])
+            return nums[i];
+       }
+       return nums[nums.length-1];
 }
+}
+ 
