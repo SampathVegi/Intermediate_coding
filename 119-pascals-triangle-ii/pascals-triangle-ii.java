8833061d316cad75c0c544af4db9f98a67d1bd47
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2025-11-10 23:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (10.08%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,20 +1,38 @@
+// class Solution {
+//     public List<Integer> getRow(int r) {
+//         List<Integer> l = new ArrayList<>();
+//         r++;
+//         int a [] = new int [r];
+
+//         a[r-1]=1;
+//         for(int i = r-2;i>=0;i--){
+//             for(int j=i;j<r-1 ;j++)
+//                 a[j]=a[j]+a[j+1];
+//             }
+
+        
+//         for(int i=0;i<r;i++){l.add(a[i]);}
+//         return l;
+    
+// }}
+
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> l = new ArrayList<>();
-        rowIndex++;
-        int a [] = new int [rowIndex];
-        // if(rowIndex<1){
-        //     l.add(1);
-        //     return l;
-        // }
-        a[rowIndex-1]=1;
-        for(int i = rowIndex-2;i>=0;i--){
-            for(int j=i;j<rowIndex-1 ;j++)
-                a[j]=a[j]+a[j+1];
+        int n = rowIndex + 1;       // total elements in the row
+        int[] row = new int[n];
+        row[0] = 1;                 // first element is always 1
+
+        // Build the row iteratively
+        for (int i = 1; i < n; i++) {
+            // Update elements from right to left
+            for (int j = i; j > 0; j--) {
+                row[j] = row[j] + row[j - 1];
             }
-            //System.out.println(a);
-        
-        for(int i=0;i<rowIndex;i++){l.add(a[i]);}
-        return l;
-    
-}}
+        }
+
+        // Convert array to list
+        List<Integer> result = new ArrayList<>();
+        for (int num : row) result.add(num);
+        return result;
+    }
+}
