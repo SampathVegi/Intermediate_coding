af0702ccde65b0519e757741bcb40527bd38b83e
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2025-11-23 01:24:26 +0530

    Time: 2 ms (83.22%) | Memory: 47.8 MB (16.28%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] moveZeroes(int[] nums) {
+        int temp=0;
+        for(int i=0,j=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;  
+                j++;          
+            }
+        }
+        return nums;
+    }
+}
