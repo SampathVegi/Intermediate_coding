7eb85b23e1050514db8fe37e9ac8c65ce1c5f4bb
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2025-11-23 12:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (30.89%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -4,9 +4,7 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                temp = nums[j];
                 nums[j]=nums[i];
-                nums[i]=temp;
                 j++;
             }
         }
