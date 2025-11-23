4d01377b74b1386092a9e6feac2070ce9a56d4ba
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2025-11-23 11:53:23 +0530

    Time: 4 ms (11.52%) | Memory: 48 MB (9.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,36 @@
-public class Solution{
-    static public void moveZeroes(int[] nums) {
-    int i = 0; // index to place next non-zero
+// class Solution {
+//     public int[] moveZeroes(int[] nums) {
+//         int temp=0;
+//         for(int i=0,j=1;i<nums.length-1;j++){
+//             if(nums[j]!=0&&nums[i]==0){
+//                 temp=nums[i];
+//                 nums[i]=nums[j];
+//                 nums[j]=temp;
+//                 i++;
+//             }
+//         }
+//         return nums;
+//     }
+// }
 
-    // Move non-zeros to the front
-    for (int j = 0; j < nums.length; j++) {
-        if (nums[j] != 0) {
-            nums[i++] = nums[j];
+
+class Solution {
+    public int[] moveZeroes(int[] nums) {
+        int temp=0;
+        for(int i=0,j=0;i<nums.length-1;j++){
+            if(nums[i]==0 && nums[j]!=0){
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+            }
+            if(nums[i]!=0){
+                i++;
+            }
+            if(j== nums.length-1){
+                return nums;
+            }
         }
+        return nums;
     }
-
-    // Fill remaining positions with zeros
-    while (i < nums.length) {
-        nums[i++] = 0;
-    }
-}
 }
