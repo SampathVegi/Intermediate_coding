548316788e27fc4a151fc46722f466dc9f23e6ad
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2026-01-02 00:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (38.26%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,10 +1,23 @@
-class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int dp[]=new int[cost.length+1];
-        int i=2 ;        
-        for(;i<dp.length;i++){
-            dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+// class Solution {
+//     public int minCostClimbingStairs(int[] cost) {
+//         int dp[]=new int[cost.length+1];
+//         int i=2 ;        
+//         for(;i<dp.length;i++){
+//             dp[i]=Math.min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+//         }
+//         return dp[i-1];
+//     }
+// }
+class Solution{
+    public int minCostClimbingStairs(int[] cost){
+        int first=0,second=0;
+
+        //if(cost.length<=2) return Math.min(first,second);
+        for(int i=2;i<=cost.length;i++){
+            int temp = Math.min(first+cost[i-2],second+cost[i-1]);
+            first=second;
+            second=temp;
         }
-        return dp[i-1];
+        return second;
     }
 }
