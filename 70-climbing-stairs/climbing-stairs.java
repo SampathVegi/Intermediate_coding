0ad808d65b8e18ca9a5190e09bd7b9c6aaee20a8
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2026-01-01 23:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (85.95%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[ ]= new int[n];
-        if(n<=2)return dp.length;
-        dp[0]=1;dp[1]=2;int i=2;
-        for(;i<n;i++){
+        int dp[ ]= new int[n+2];
+        if(n<=2)return n;
+        dp[1]=1;dp[2]=2;int i=3;
+        for(;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
-        return dp[i-1];
+        return dp[n];
     }
 }
