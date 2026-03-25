994566a3ed94edb01b634d06ffb55b8e6149ced6
SampathVegi <114206981+SampathVegi@users.noreply.github.com>
2026-03-25 16:44:29 +0530

    Time: 7 ms (81.40%) | Memory: 19.6 MB (70.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,8 @@
-import re
 class Solution:
-
     def isPalindrome(self,s: str) -> bool:
-        # remove non-alphanumeric characters
-        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s)
-        
-        # convert to lowercase
-        cleaned = cleaned.lower()
-        
-        # check palindrome
-        return cleaned == cleaned[::-1]
+        a=''
+        for i in s:
+            if i.isalnum():
+                a+=i
+        a=a.lower()
+        return a==a[::-1]
